e2d7211d50232a88157f2a7e6248a529da0234bd
OlehSharha <oleh.3sharga@gmail.com>
2023-07-13 17:39:11 +0300

    impoved code readability

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,34 @@
 import React, { useState, useEffect } from 'react';
 
-const PasswordStrengthChecker = () => {
+const App = () => {
   const [password, setPassword] = useState('');
   const [colors, setColors] = useState(['gray', 'gray', 'gray']);
 
-  useEffect(() => {
+  const updateColors = () => {
     const hasLetters = /[a-zA-Z]/.test(password);
     const hasDigits = /\d/.test(password);
     const hasSymbols = /[!@#$%^&*()\-=_+[\]{};':"\\|,.<>/?]/.test(password);
 
-    if (password.length > 0 && password.length < 8) {
-      setColors(['red', 'red', 'red']);
-    } else if (hasLetters && hasDigits && hasSymbols) {
-      return setColors(['green', 'green', 'green']);
-    }else if ((hasLetters && hasSymbols) || (hasLetters && hasDigits) || (hasDigits && hasSymbols)) {
-      return setColors(['yellow', 'yellow', 'gray']);
-    } else if (password.length >= 8 && (hasDigits || hasLetters || hasSymbols)) {
-      setColors(['red', 'gray', 'gray'])
+    switch (true) {
+      case password.length > 0 && password.length < 8:
+        setColors(['red', 'red', 'red']);
+        break;
+      case hasLetters && hasDigits && hasSymbols:
+        setColors(['green', 'green', 'green']);
+        break;
+      case (hasLetters && hasSymbols) || (hasLetters && hasDigits) || (hasDigits && hasSymbols):
+        setColors(['yellow', 'yellow', 'gray']);
+        break;
+      case password.length >= 8 && (hasDigits || hasLetters || hasSymbols):
+        setColors(['red', 'gray', 'gray']);
+        break;
+      default:
+        setColors(['gray', 'gray', 'gray']);
     }
-     else {
-      setColors(['gray', 'gray', 'gray']);
-    }
+  };
+
+  useEffect(() => {
+    updateColors();
   }, [password]);
 
   return (
@@ -35,7 +43,7 @@ const PasswordStrengthChecker = () => {
       {colors.map((color) => {
         return (
           <div
-            style={{ background: color, height: '10px', margin: '10px 0' }}
+            style={{ background: color, height: '10px', margin: '10px 0', width: '175px' }}
           ></div>
         );
       })}
@@ -43,4 +51,4 @@ const PasswordStrengthChecker = () => {
   );
 };
 
-export default PasswordStrengthChecker;
+export default App;
