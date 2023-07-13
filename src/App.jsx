332231fc0f0a2db1668e887cce4ba247e599b196
OlehSharha <oleh.3sharga@gmail.com>
2023-07-13 17:49:21 +0300

    added more information about pass

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from 'react';
 
 const App = () => {
   const [password, setPassword] = useState('');
-  const [colors, setColors] = useState(['gray', 'gray', 'gray']);
+  const [colors, setColors] = useState([]);
+  const [message, setMessage] = useState('');
 
   const updateColors = () => {
     const hasLetters = /[a-zA-Z]/.test(password);
@@ -12,18 +13,23 @@ const App = () => {
     switch (true) {
       case password.length > 0 && password.length < 8:
         setColors(['red', 'red', 'red']);
+        setMessage('Your password is WEAK, upgrade please!');
         break;
       case hasLetters && hasDigits && hasSymbols:
         setColors(['green', 'green', 'green']);
+        setMessage('Your password is STRONG, I knew you can do it!');
         break;
       case (hasLetters && hasSymbols) || (hasLetters && hasDigits) || (hasDigits && hasSymbols):
         setColors(['yellow', 'yellow', 'gray']);
+        setMessage('Your password is MEDIUM, you can better I believe!');
         break;
       case password.length >= 8 && (hasDigits || hasLetters || hasSymbols):
         setColors(['red', 'gray', 'gray']);
+        setMessage('Your password is EASY, you can better I believe!');
         break;
       default:
         setColors(['gray', 'gray', 'gray']);
+        setMessage('Enter a password :)');
     }
   };
 
@@ -44,9 +50,10 @@ const App = () => {
         return (
           <div
             style={{ background: color, height: '10px', margin: '10px 0', width: '175px' }}
-          ></div>
+          />
         );
       })}
+      <div>{message}</div>
     </>
   );
 };
